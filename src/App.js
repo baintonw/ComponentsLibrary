5a9a414ae3d9f2df8919530f0ae1831d875fb240
Will Bainton <baintonw@gmail.com>
2020-08-22 17:53:54 -0400

    stand in template added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+// import 'node-sass';
+//Components
+import SignIn from './Components/SignIn'
+
 function App() {
   return (
     <div className="App">
-     {"<---->"}
+      <SignIn></SignIn>
     </div>
   );
 }
---
+++ src/Components/SignIn.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+//Styles
+import './signin.scss'
+
+const SignIn = () => {
+    return(
+        <div className="signIn">
+            <div class="container">
+		    <div class="box">
+			<input type="checkbox" id="toggle" class="box__toggle" hidden />
+			<img src="https://source.unsplash.com/zv3ckJKftC4/300x400" alt="Picture by Autumn Studio" class="box__image" />
+			<form class="form form--register" action="">
+				<h1 class="form__title">Sign up</h1>
+
+				<div class="form__helper">
+					<input type="text" name="user" id="new-user" placeholder="User" class="form__input" />
+					<label class="form__label" for="new-user">User</label>
+				</div>
+
+				<div class="form__helper">
+					<input type="email" name="email" id="email" placeholder="Email" class="form__input" />
+					<label class="form__label" for="email">Email</label>
+				</div>
+
+				<div class="form__helper">
+					<input type="password" name="password" id="new-user-password" placeholder="Password" class="form__input" />
+					<label class="form__label" for="new-user-password">Password</label>
+				</div>
+
+				<div class="form__helper">
+					<input type="password" name="password" id="confirm-password" Placeholder="Confirm password" class="form__input" />
+					<label class="form__label" for="confirm-password">Confirm password</label>
+				</div>
+
+				<button type="submit" class="form__button">Register</button>
+
+				<p class="form__text">Already have an account?</p> <label for="toggle" class="form__link">Sign in!</label>
+			</form>
+
+			<form class="form form--login" action="">
+				<h1 class="form__title">Sign in</h1>
+
+				<div class="form__helper">
+					<input type="text" name="user" id="user" placeholder="User" class="form__input" />
+					<label class="form__label" for="user">User</label>
+				</div>
+
+				<div class="form__helper">
+					<input type="password" name="password" id="password" placeholder="Password" class="form__input" />
+					<label class="form__label" for="password">Password</label>
+				</div>
+
+				<button type="submit" class="form__button">Login</button>
+
+				<p class="form__text">Don't have an account?</p><label for="toggle" class="form__link">Sign up!</label>
+			</form>
+		</div>
+	</div>
+        </div>
+    )
+}
+
+export default SignIn;
