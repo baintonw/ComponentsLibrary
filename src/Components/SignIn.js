610afdafccd75896a9a9c6d6249a379ecccbff07
Will Bainton <baintonw@gmail.com>
2020-08-24 13:25:10 -0400

    image toggle functioning properly

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -7,8 +7,9 @@ const SignIn = () => {
     return(
         <div className="signIn">
             <div className="box">
-                <input type="checkbox" id="toggle" className="box__toggle"></input>
-                <img src="https://picsum.photos/300"/>
+                <input type="checkbox" id="toggle" className="box__toggle" hidden/>
+                    <img className="box__image" src="https://picsum.photos/300"/>
+                
                 {/* Sign up form */}
                 <form className="form form__register" action="">
                     <h1 className="form__title">Sign Up</h1>
@@ -19,25 +20,28 @@ const SignIn = () => {
                     </div>
                     {/* Email */}
                     <div className="form__helper">
-                        <input className="form__label" type="email" name="email" id="email" placeholder="Email"></input>
+                        <input className="form__input" type="email" name="email" id="email" placeholder="Email"></input>
                         <label className="form__label" for="email">Email</label>
                     </div>
-
                     {/* Password */}
                     <div className="form__helper">
-                        <input className="form__label" type="password" name="password" id="new-user-password"></input>
-                        <label for="new-user-password">Password</label>
+                        <input className="form__input" type="password" name="password" id="new-user-password"></input>
+                        <label className="form__label" for="new-user-password">Password</label>
                     </div>
                     {/* Confirm Password */}
                     <div className="form__helper">
-                        <input className="form__label" type="password" name="password" id="confirm-password"></input>
-                        <label for="new-user-password">Confirm Password</label>
+                        <input className="form__input" type="password" name="password" id="confirm-password"></input>
+                        <label className="form__label" for="new-user-password">Confirm Password</label>
                     </div>
+
                     {/* Submit Button */}
+
                     <button type="submit" className="form__button">Register</button>
                     <p className="form__text">Already have an account?</p>
-                    <label className="form__link">Sign In!</label>
+                    <label for="toggle" className="form__link">Sign In!</label>
                 </form>
+
+                {/* ---------------------- */}
 
                 {/* Sign In form (default) */}
                 <form className="form form__register" action="">
@@ -51,14 +55,15 @@ const SignIn = () => {
 
                     {/* Password */}
                     <div className="form__helper">
-                        <input className="form__label" type="password" name="password" id="new-user-password"></input>
-                        <label for="new-user-password">Password</label>
+                        <input className="form__input" type="password" name="password" id="new-user-password"
+                                placeholder="Password"></input>
+                        <label className="form__label" for="new-user-password">Password</label>
                     </div>
 
                     {/* Submit Button */}
                     <button type="submit" className="form__button">Log in</button>
                     <p className="form__text">Don't have an account?</p>
-                    <label className="form__link">Sign Up!</label>
+                    <label for="toggle" className="form__link">Sign Up!</label>
                 </form>
 
             </div>
