9732a98f351b9c78b06f5a2058c58b23c658eb49
Will Bainton <baintonw@gmail.com>
2020-08-25 19:16:41 -0400

    card templating

--- src/Components/FoodCard.js
+++ src/Components/FoodCard.js
@@ -7,13 +7,18 @@ const FoodCard = () => {
     return (
         <div className="container">
             <div className="foodCard">
-                <img src="https://picsum.photos/300"></img>
-                <h2 className="foodCard__name">Bananas</h2>
-                <p className="foodCard__quantity">#5</p>
-                <p className="foodCard__who">Who</p>
-                <p className="foodCard__what">What</p>
-                <p className="foodCard__location">Location</p>
-                <p className="foodCard__description">Description</p>
+                <img className="foodCard__image" src="https://picsum.photos/200"></img>
+                <div className="foodCard__info-box">
+                    <p className="foodCard__name">Bananas<p className="foodCard__quantity">#5</p></p>
+                    <p className="foodCard__who">Who</p>
+                    <p className="foodCard__what">What</p>
+                    <p className="foodCard__location">Location</p>
+                </div>
+                <div className="foodCard__description">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque purus urna, faucibus nec placerat ac, semper eget leo. Cras sem massa, molestie vitae iaculis vitae, rutrum euismod leo. Vivamus faucibus libero arcu, feugiat porttitor mi luctus eget. Phasellus gravida posuere metus et euismod. Donec semper luctus tortor, eget lacinia sem.
+                    </p>
+                </div>
             </div>
         </div>
     )
