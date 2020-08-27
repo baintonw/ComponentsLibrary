1d2584b79511b734c02ed9d23266a26ed99abcf0
Will Bainton <baintonw@gmail.com>
2020-08-26 22:43:12 -0400

    card template set, not responsive

--- src/Components/FoodCard.js
+++ src/Components/FoodCard.js
@@ -9,7 +9,7 @@ const FoodCard = () => {
             <div className="foodCard">
                 <img className="foodCard__image" src="https://picsum.photos/200"></img>
                 <div className="foodCard__info-box">
-                    <p className="foodCard__name">Bananas<p className="foodCard__quantity">#5</p></p>
+                    <p className="foodCard__name">Bananas <span>#5</span></p>
                     <p className="foodCard__who">Who</p>
                     <p className="foodCard__what">What</p>
                     <p className="foodCard__location">Location</p>
@@ -19,6 +19,7 @@ const FoodCard = () => {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque purus urna, faucibus nec placerat ac, semper eget leo. Cras sem massa, molestie vitae iaculis vitae, rutrum euismod leo. Vivamus faucibus libero arcu, feugiat porttitor mi luctus eget. Phasellus gravida posuere metus et euismod. Donec semper luctus tortor, eget lacinia sem.
                     </p>
                 </div>
+                <button className="foodCard__button">Expand</button>
             </div>
         </div>
     )
